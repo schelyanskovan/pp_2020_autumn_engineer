19d78052404cc29b3edc8680b606a4a3dbb43b34
igor_rukhovich <ruhovich@mail.ru>
2020-12-12 03:19:12 +0300

    Task 3 init

---
+++ modules/task_3/rukhovich_i_radix_sort_batcher/main.cpp
@@ -0,0 +1,22 @@
+// Copyright 2020 Igor Rukhovich
+#include <gtest-mpi-listener.hpp>
+#include <gtest/gtest.h>
+#include <vector>
+#include "./radix_sort_batcher.h"
+
+
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    MPI_Init(&argc, &argv);
+
+    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+    ::testing::TestEventListeners& listeners =
+        ::testing::UnitTest::GetInstance()->listeners();
+
+    listeners.Release(listeners.default_result_printer());
+    listeners.Release(listeners.default_xml_generator());
+
+    listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+    return RUN_ALL_TESTS();
+}
---
+++ modules/task_3/rukhovich_i_radix_sort_batcher/radix_sort_batcher.cpp
@@ -0,0 +1,3 @@
+// Copyright 2020 Igor Rukhovich
+#include "../../modules/task_3/rukhovich_i_radix_sort_batcher/radix_sort_batcher.h"
+#include <mpi.h>
