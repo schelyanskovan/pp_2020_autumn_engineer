f6469b3431c2d2a2745dfc85e7b64e197fa097d5
ZlobinGM <geozlo8738@gmail.com>
2020-12-15 19:31:32 +0300

    v1

---
+++ modules/task_3/zlobin_g_shortest_paths_moore_algorithm/moore_algorithm.h
@@ -0,0 +1,12 @@
+// Copyright 2020 Zlobin George
+#ifndef MODULES_TASK_3_ZLOBIN_G_SHORTEST_PATHS_MOORE_ALGORITHM_MOORE_ALGORITHM_H_
+#define MODULES_TASK_3_ZLOBIN_G_SHORTEST_PATHS_MOORE_ALGORITHM_MOORE_ALGORITHM_H_
+
+#define MAX_VALUE 20
+#define EDGE_PROB 0.2
+
+void fillGraphWithRandomValues(int* graph_to_fill, size_t size = 10);
+void getSequentialMooreAlgorithm(int* graph, size_t size, int* shortest_ways, size_t begin_vertex = 0);
+void getParallelMooreAlgorithm(int* graph, size_t size, int* shortest_ways, size_t begin_vertex);
+
+#endif  // MODULES_TASK_3_ZLOBIN_G_SHORTEST_PATHS_MOORE_ALGORITHM_MOORE_ALGORITHM_H_
