5947299e678b71d5439d603c3440d0b4bdc2af41
Igor <kdikasz@mail.ru>
2020-12-17 00:53:54 +0300

    Version 3. Task 7

--- modules/task_3/rachin_i_integ_by_rect/main.cpp
+++ modules/task_3/rachin_i_integ_by_rect/main.cpp
@@ -9,7 +9,7 @@ TEST(Parallel_Operations_MPI, Test_manual_integ) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     if (rank == 0) {
         res1 = getSequentialInteg(f1, -1.0, 0.0, 0.0, 2.0, 0.0, 1.0, 0.1, 0.1, 0.1);
-        ASSERT_LE(fabs(res1-4.98168), 0.01);  // due to step in 0.1
+        ASSERT_NEAR(res1, 4.98168, 0.01);  // due to step in 0.1
     }
 }
 
@@ -19,7 +19,7 @@ TEST(Parallel_Operations_MPI, Test_parallel_integ) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     res1 = getParallelInteg(f1, -1.0, 0.0, 0.0, 2.0, 0.0, 1.0, 0.1, 0.1, 0.1);
     if (rank == 0) {
-        ASSERT_LE(fabs(res1 - 4.98168), 0.01);  // due to step in 0.1
+        ASSERT_NEAR(res1, 4.98168, 0.01);  // due to step in 0.1
     }
 }
 
@@ -30,7 +30,7 @@ TEST(Parallel_Operations_MPI, Test_parallel_with_manual) {
     res1 = getParallelInteg(f1, -1.0, 0.0, 0.0, 2.0, 0.0, 1.0, 0.1, 0.1, 0.1);
     if (rank == 0) {
         res2 = getSequentialInteg(f1, -1.0, 0.0, 0.0, 2.0, 0.0, 1.0, 0.1, 0.1, 0.1);
-        ASSERT_LE(fabs(res1 - res2), 0.001);  // due to step in 0.1
+        ASSERT_NEAR(res1, res2, 0.001);  // due to step in 0.1
     }
 }
 
@@ -41,7 +41,7 @@ TEST(Parallel_Operations_MPI, Test_second_func) {
     res1 = getParallelInteg(f2, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.1, 0.1, 0.1);
     if (rank == 0) {
         res2 = getSequentialInteg(f2, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.1, 0.1, 0.1);
-        ASSERT_LE(fabs(res1 - res2), 0.001);  // due to step in 0.1
+        ASSERT_NEAR(res1, res2, 0.001);  // due to step in 0.1
     }
 }
 
@@ -52,7 +52,7 @@ TEST(Parallel_Operations_MPI, Test_third_func) {
     res1 = getParallelInteg(f3, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.1, 0.1, 0.1);
     if (rank == 0) {
         res2 = getSequentialInteg(f3, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.1, 0.1, 0.1);
-        ASSERT_LE(fabs(res1 - res2), 0.001);  // due to step in 0.1
+        ASSERT_NEAR(res1, res2, 0.001);  // due to step in 0.1
     }
 }
 
