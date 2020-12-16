50c89df0301305db4f7f3995888971458da76d63
Alexander <sharoffalex@gmail.com>
2020-12-16 22:59:43 +0300

    1337

---
+++ modules/task_3/sharov_a_shell/shell.h
@@ -0,0 +1,11 @@
+// Copyright 2020 Sharov Alexander
+#ifndef MODULES_TASK_3_SHAROV_A_SHELL_SHELL_H_
+#define MODULES_TASK_3_SHAROV_A_SHELL_SHELL_H_
+#include <vector>
+
+std::vector<int> sortshell(std::vector<int> a, int n);
+std::vector<int> merge(std::vector<int> a, std::vector<int> b);
+std::vector<int> genvector(int size);
+std::vector<int> parallelsortshell(std::vector<int> vec, int n);
+
+#endif  //  MODULES_TASK_3_SHAROV_A_SHELL_SHELL_H_
