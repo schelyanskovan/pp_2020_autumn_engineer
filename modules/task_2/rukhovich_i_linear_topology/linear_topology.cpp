43c0cdd61fbcad2441ee42f339a49d7734967ba1
igor_rukhovich <ruhovich@mail.ru>
2020-12-26 01:44:11 +0300

    Linear topology has been done

--- modules/task_2/rukhovich_i_linear_topology/linear_topology.cpp
+++ modules/task_2/rukhovich_i_linear_topology/linear_topology.cpp
@@ -1,62 +1,14 @@
 // Copyright 2020 Igor Rukhovich
 #include "../../modules/task_2/rukhovich_i_linear_topology/linear_topology.h"
 #include <mpi.h>
-#include <random>
-#include <vector>
 
-int RandomInt::Next() {
-    static RandomInt rand = RandomInt();
-    return rand.dist_(rand.gen_);
-}
+MPI_Comm make_linear_topology() {
+    int num_proc;
+    MPI_Comm_size(MPI_COMM_WORLD, &num_proc);
 
-RandomInt::RandomInt() : gen_(std::random_device()()), dist_(-1000, 1000) {}
-
-std::vector<int> random_integral_array(size_t size) {
-    std::vector<int> arr(size);
-
-    for (size_t i = 0; i < size; ++i) {
-        arr[i] = RandomInt::Next();
-    }
-
-    return arr;
-}
-
-MPI_Comm linear_topology(int* order) {
-    int ProcRank, ProcNum;
-    MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
-    MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
-
-    int *index = new int[ProcNum];
-    for (int proc = 0; proc < ProcNum; proc++) {
-        index[proc] = 2 * (proc + 1);
-    }
-
-    int *edges = new int[ProcNum * 2];
-    if (order != NULL) {
-        int left, right;
-        for (int i = 0; i < ProcNum; i++) {
-            if (i == 0) {
-                left = order[ProcNum-1];
-            } else {
-                left = order[i - 1];
-            }
-            if (i == ProcNum - 1) {
-                right = order[0];
-            } else {
-                right = order[i + 1];
-            }
-            edges[2 * order[i]] = left;
-            edges[2 * order[i] + 1] = right;
-        }
-    } else {
-        for (int i = 0; i < ProcNum; i++) {
-            edges[(2 + (i * 2)) % (ProcNum * 2)] = i;
-            edges[(ProcNum * 2 - 1 + i * 2) % (ProcNum * 2)] = i;
-        }
-    }
-
-    MPI_Comm RingComm;
-    MPI_Graph_create(MPI_COMM_WORLD, ProcNum, index, edges, true, &RingComm);
-
-    return RingComm;
+    MPI_Comm linear_comm;
+    const int dims[1] = { num_proc };
+    const int periods[1] = { false };
+    MPI_Cart_create(MPI_COMM_WORLD, 1, dims, periods, false, &linear_comm);
+    return linear_comm;
 }
--- modules/task_2/rukhovich_i_linear_topology/linear_topology.h
+++ modules/task_2/rukhovich_i_linear_topology/linear_topology.h
@@ -3,22 +3,7 @@
 #define MODULES_TASK_2_RUKHOVICH_I_LINEAR_TOPOLOGY_LINEAR_TOPOLOGY_H_
 
 #include <mpi.h>
-#include <random>
-#include <vector>
 
-class RandomInt {
- public:
-    static int Next();
-
- private:
-    RandomInt();
-
-    std::mt19937_64 gen_;
-    std::uniform_int_distribution<int> dist_;
-};
-
-std::vector<int> random_integral_array(size_t size);
-
-MPI_Comm linear_topology(int* order);
+MPI_Comm make_linear_topology();
 
 #endif  // MODULES_TASK_2_RUKHOVICH_I_LINEAR_TOPOLOGY_LINEAR_TOPOLOGY_H_
